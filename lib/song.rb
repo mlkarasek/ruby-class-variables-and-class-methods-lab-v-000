b1864fe3d4bb8733b81f3acb14b8b20e8ba2b4d8
Monica  Karasek <mlkarasek@gmail.com>
2017-10-26 20:26:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ class Song
 attr_accessor :count, :artists, :genres
 
 @@count = 0
+@@genres = 0
+@@artists = 0
 
 def initialize(count, artists, genres)
   @count = count
@@ -10,6 +12,17 @@ def initialize(count, artists, genres)
   @genres = genres
 
   @@count += 1
+  @@genres += 1
+  @@artists +=1
+end
+
+def count 
+end 
+
+def genres
+end 
+
+def artists
 end 
 
 end
