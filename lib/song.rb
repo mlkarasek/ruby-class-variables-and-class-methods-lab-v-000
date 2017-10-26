e090b3c000e2a2120b6f04a3cecb28c4283d4b9e
Monica  Karasek <mlkarasek@gmail.com>
2017-10-26 20:39:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,41 +1,43 @@
+require 'pry'
+
 class Song
 
-attr_accessor :name, :artists, :genres
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
-@@count = 0
-@@genres = []
-@@artists = []
+  def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
 
-def initialize(song_name, artists, genres)
-  @name = song_name
-  @artists = artists
-  @genres = genress
-  @@artists << artists
-  @@genres << genres
-  @@count += 1
-end
+  def self.count 
+    @@count
+  end
 
-def self.count
-  @@count
-end
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.genres
-  @@genres.uniq
-end
+  def self.genres
+    @@genres.uniq
+  end
 
-def self.artists
-  @@artists.uniq
-end
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
 
-def self.genre_count
-  genre_count = Hash.new
-  @@genres.each { |genres| genre_count[genres] += 1 }
-  genre_count
-end
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
 
-def self.artist_count
-  artist_count = Hash.new
-  @@artists.each { |artist| artist_count[artist] += 1 }
-  artist_count
-end 
 end
